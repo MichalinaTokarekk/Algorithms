49dca44df40738fd8a6ffbc6a3d5a89a779dde99
MichalinaTokarek <michalina.tokarek@gmail.com>
2024-07-09 15:27:29 +0200

    Create RSA.py

---
+++ Python/cryptographic/RSA.py
@@ -0,0 +1,50 @@
+import random
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def modinv(a, m):
+    m0, x0, x1 = m, 0, 1
+    if m == 1:
+        return 0
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += m0
+    return x1
+
+def generate_keypair(p, q):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    d = modinv(e, phi)
+    return ((e, n), (d, n))
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+def decrypt(pk, ciphertext):
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    return ''.join(plain)
+
+# Przykład użycia
+p = 61
+q = 53
+public, private = generate_keypair(p, q)
+message = "HELLO"
+encrypted_msg = encrypt(public, message)
+decrypted_msg = decrypt(private, encrypted_msg)
+print("RSA Algorithm - Original message:", message)
+print("RSA Algorithm - Encrypted message:", encrypted_msg)
+print("RSA Algorithm - Decrypted message:", decrypted_msg)
